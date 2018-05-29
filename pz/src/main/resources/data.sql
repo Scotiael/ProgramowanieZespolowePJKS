9d4359af88a4cfeae4bf8752e5428146af38cd03
Kamil Bargiel <scotiaelwmv@gmail.com>
2018-05-29 22:14:01 +0200

    Restore functionality connected with inserting default values to tables in db on app start

--- pz/src/main/resources/data.sql
+++ pz/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into role VALUES(1, 'admin');
-insert into role VALUES(2, 'user');
-insert into user VALUES(1, 'admin', 'admin', '$2a$10$IGPMWuTMlO9mF59ZLbW2geaTeqaR0m6.iFk7UofcT7QhJAXJO3ZQi', 'admin', 1);
+insert IGNORE into role VALUES(1, 'admin');
+insert IGNORE into role VALUES(2, 'user');
+insert IGNORE into user VALUES(1, 'admin', 'admin', '$2a$10$IGPMWuTMlO9mF59ZLbW2geaTeqaR0m6.iFk7UofcT7QhJAXJO3ZQi', 'admin', 1);
